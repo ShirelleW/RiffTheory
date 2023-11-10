8236bac9c6ff8b6c439b96aee408a41cdff5991a
ShirelleW <shirellewinecoffm@gmail.com>
2023-11-09 22:18:10 -0500

    Added slider control and text

--- pages/Slider.jsx
+++ pages/Slider.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import styles from '../styles/Styles.module.css'
 import Draggable from 'react-draggable'
 
-const Slider = ( {sliderFretRange, numOfStrings} ) => {
+const Slider = ({ sliderFretRange, numOfStrings }) => {
     return (
-        <Draggable
-         bounds='parent'>
-        <div className={styles.slider} style={{ width: (3.15 * sliderFretRange) + 'vw', height: (7 * numOfStrings) + 'vh' }}>
-        </div>
+        <Draggable bounds='parent'>
+                <div className={styles.slider} style={{ width: (3.15 * sliderFretRange) + 'vw', height: (7 * numOfStrings) + 'vh' }}>
+                    Drag Me!
+                </div>
         </Draggable>
     )
 }
